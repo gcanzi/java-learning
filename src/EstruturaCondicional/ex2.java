8be9432bb989e8a6410f6f2f6c16b8eb9bd74667
Gustavo Moretim Canzi <gustavo.canzi@gmail.com>
2026-01-05 22:31:58 -0300

    practicing

---
+++ src/EstruturaCondicional/ex2.java
@@ -0,0 +1,24 @@
+//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+package EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Digite um número: ");
+		int num = sc.nextInt();
+		
+		if (num % 2 == 0) {
+			System.out.println("O número digitado é par!");
+		}
+		else {
+			System.out.println("O número digitado é impar!");
+		}
+		
+		sc.close();
+	}
+}
---
+++ src/EstruturaCondicional/ex3.java
@@ -0,0 +1,36 @@
+//Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se
+//os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em ordem crescente ou decrescente.
+
+package EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class ex3 {
+
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Digite dois números:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if (a >= b) {
+			if (a % b == 0) {
+				System.out.println("São Multiplos!");
+			}
+			else {
+				System.out.println("Não são Multiplos!");
+			}
+		}
+		else {
+			if (b % a == 0) {
+				System.out.println("São Multiplos!");
+			}
+			else {
+				System.out.println("Não são Multiplos!");
+			}
+		}
+		
+		sc.close();		
+	}
+}
