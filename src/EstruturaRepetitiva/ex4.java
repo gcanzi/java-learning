cc27ff925d9c443256ea5a91c079b39dea032db2
Gustavo Moretim Canzi <gustavo.canzi@gmail.com>
2026-01-06 23:13:03 -0300

    practicing

---
+++ src/EstruturaRepetitiva/ex4.java
@@ -0,0 +1,22 @@
+//Leia um valor inteiro X. Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+package EstruturaRepetitiva;
+
+import java.util.Scanner;
+
+public class ex4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Digite um numero: ");
+		int num = sc.nextInt();
+		
+		for (int i=1 ; i<=num; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+	}
+}
---
+++ src/EstruturaRepetitiva/ex9.java
@@ -0,0 +1,22 @@
+//Ler um número inteiro N e calcular todos os seus divisores.
+
+package EstruturaRepetitiva;
+
+import java.util.Scanner;
+
+public class ex9 {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Digite um numero para descobrir seus divisores: ");
+		int x = sc.nextInt();
+		
+		for (int i=1 ; i<=x ; i++){
+			if (x % i == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+	}
+}
