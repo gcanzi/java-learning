d4ac7a1e22f240a78cb4aab2fed4a04108fcc400
Gustavo Moretim Canzi <gustavo.canzi@gmail.com>
2026-01-09 16:51:42 -0300

    practicing

---
+++ src/poo/media/Main.java
@@ -0,0 +1,27 @@
+package poo.media;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		aluno alu = new aluno();
+		
+		System.out.print("Digite seu nome: ");
+		alu.nome = sc.nextLine();
+		System.out.print("Digite suas 3 notas: ");
+		alu.nota1 = sc.nextDouble();
+		alu.nota2 = sc.nextDouble();
+		alu.nota3 = sc.nextDouble();
+		
+		if (alu.mediaFinal() < 60) {
+			System.out.println("Reprovado!");
+			System.out.printf("Faltou %.2f pontos", alu.faltouNota());
+		} else {
+			System.out.println("Aprovado!");
+		}
+		
+		sc.close();
+	}
+}
---
+++ src/poo/media/aluno.java
@@ -0,0 +1,20 @@
+package poo.media;
+
+public class aluno {
+	public String nome;
+	public double nota1;
+	public double nota2;
+	public double nota3;
+	
+	public double mediaFinal() {
+		return nota1 + nota2 + nota3; 
+	}
+	
+	public double faltouNota() {
+		if (mediaFinal() < 60.0) {
+			return 60 - mediaFinal();
+		} else {
+			return 0;
+		}
+	}
+}
