30cb41ed102e2952ec0b2916164f2533925e7177
Gustavo Moretim Canzi <gustavo.canzi@gmail.com>
2026-01-10 10:29:39 -0300

    practicing

--- src/poo/media/Main.java
+++ src/poo/media/Main.java
@@ -1,9 +1,11 @@
 package poo.media;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		aluno alu = new aluno();
