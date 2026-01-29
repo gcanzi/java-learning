9c4d648bf02ebbd8364b487879428c25ea2e76bd
Gustavo Moretim Canzi <gustavo.canzi@gmail.com>
2026-01-28 23:23:20 -0300

    practicing

---
+++ src/poo/polimorfismo/itens/Program.java
@@ -0,0 +1,55 @@
+package poo.polimorfismo.itens;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		List<Product> list = new ArrayList<>();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Product #" + i + " data:");
+			System.out.print("Common, used or imported (c/u/i)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			String name = sc.next();
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+			
+			if (ch == 'u') {
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date manufacture = sdf.parse(sc.next());
+				list.add(new UsedProduct(name, price, manufacture));
+			}
+			else if (ch == 'i') {
+				System.out.print("Customs fee: ");
+				double cfee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, cfee));
+			}
+			else {
+				list.add(new Product(name, price));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("Price Tags:");
+		
+		for (Product pro : list) {
+			System.out.println(pro.priceTag());
+		}
+		
+		sc.close();
+	}
+
+}
