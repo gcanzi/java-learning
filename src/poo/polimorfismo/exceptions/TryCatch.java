7d1eebe19807067c5065c433e23f6d5b404139c2
Gustavo Moretim Canzi <gustavo.canzi@gmail.com>
2026-02-03 23:01:41 -0300

    practicing

---
+++ src/poo/polimorfismo/exceptions/TryCatch.java
@@ -0,0 +1,22 @@
+package poo.polimorfismo.exceptions;
+
+import java.util.Scanner;
+
+public class TryCatch {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);	
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+		}
+		
+		
+		sc.close();
+	}
+}
